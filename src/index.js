b87ba4532bdba5c4257ae7d66d4e1ba32b1c4bb9
Arun Sood <arsood@gmail.com>
2018-04-19 16:45:33 -0700

    Basic router configuration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
+// React Router 4 modules
+import { Router, Route, Switch } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+
 import UserList from "./UserList";
 
 // Optimize production bundle
 registerServiceWorker();
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <UserList />,
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" component={UserList} />
+    </Switch>
+  </Router>
+  ,
   document.getElementById("root")
 );
