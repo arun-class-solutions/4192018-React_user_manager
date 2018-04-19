9cf9f83507df359c80db9f05484652a37b9ae9ba
Arun Sood <arsood@gmail.com>
2018-04-19 16:34:37 -0700

    Add new user via POST request

--- src/UserList.js
+++ src/UserList.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import axios from "axios";
+import update from "immutability-helper";
 
 class UserList extends Component {
   constructor() {
     super();
 
     this.state = {
-      users: []
+      users: [],
+      newUser: {
+        firstname: "",
+        lastname: "",
+        username: "",
+        email: ""
+      }
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -17,6 +27,31 @@ class UserList extends Component {
       // To get body of response, it is response.data
       this.setState({
         users: response.data
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  handleChange(event) {
+    this.setState(update(this.state, {
+      newUser: {
+        $merge: {
+          [event.target.name]: event.target.value
+        }
+      }
+    }));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    axios
+    .post("https://myapi-profstream.herokuapp.com/api/403281/persons", this.state.newUser)
+    .then((response) => {
+      this.setState({
+        users: this.state.users.concat(response.data)
       });
     })
     .catch((err) => {
@@ -76,40 +111,42 @@ class UserList extends Component {
         <div id="add-user-modal" className="modal fade">
         	<div className="modal-dialog">
         		<div className="modal-content">
-        			<div className="modal-header">
-        				<button type="button" className="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
-        				<h4 className="modal-title">Add a User</h4>
-        			</div>
-        			<div className="modal-body">
-        				<div className="bold">
-        					First Name
-        				</div>
-        				<div className="margin-top-20">
-        					<input name="firstname" type="text" className="form-control" placeholder="First Name" />
-        				</div>
-        				<div className="bold margin-top-20">
-        					Last Name
-        				</div>
-        				<div className="margin-top-20">
-        					<input name="lastname" type="text" className="form-control" placeholder="Last Name" />
-        				</div>
-        				<div className="bold margin-top-20">
-        					Username
-        				</div>
-        				<div className="margin-top-20">
-        					<input name="username" type="text" className="form-control" placeholder="Username" />
-        				</div>
-        				<div className="bold margin-top-20">
-        					Email
-        				</div>
-        				<div className="margin-top-20">
-        					<input name="email" type="text" className="form-control" placeholder="Email" />
-        				</div>
-        			</div>
-        			<div className="modal-footer">
-        				<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-        				<button type="submit" className="btn btn-primary">Save User</button>
-        			</div>
+              <form onSubmit={this.handleSubmit}>
+          			<div className="modal-header">
+          				<button type="button" className="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
+          				<h4 className="modal-title">Add a User</h4>
+          			</div>
+          			<div className="modal-body">
+          				<div className="bold">
+          					First Name
+          				</div>
+          				<div className="margin-top-20">
+          					<input onChange={this.handleChange} name="firstname" type="text" className="form-control" placeholder="First Name" />
+          				</div>
+          				<div className="bold margin-top-20">
+          					Last Name
+          				</div>
+          				<div className="margin-top-20">
+          					<input onChange={this.handleChange} name="lastname" type="text" className="form-control" placeholder="Last Name" />
+          				</div>
+          				<div className="bold margin-top-20">
+          					Username
+          				</div>
+          				<div className="margin-top-20">
+          					<input onChange={this.handleChange} name="username" type="text" className="form-control" placeholder="Username" />
+          				</div>
+          				<div className="bold margin-top-20">
+          					Email
+          				</div>
+          				<div className="margin-top-20">
+          					<input onChange={this.handleChange} name="email" type="text" className="form-control" placeholder="Email" />
+          				</div>
+          			</div>
+          			<div className="modal-footer">
+          				<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+          				<button type="submit" className="btn btn-primary">Save User</button>
+          			</div>
+              </form>
         		</div>
         	</div>
         </div>
