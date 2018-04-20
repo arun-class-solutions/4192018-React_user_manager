98bb4e177b6d3999d1dcc9c638a876d5d07faf62
Arun Sood <arsood@gmail.com>
2018-04-20 13:02:20 -0700

    Added snapshot spec

---
+++ src/__tests__/UserList-spec.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import UserList from "../UserList";
+
+import MockAdapter from "axios-mock-adapter";
+const mockHttp = new MockAdapter();
+
+configure({adapter: new Adapter()});
+
+describe("UserList Component", () => {
+
+  it("Should render UserList", () => {
+    mockHttp
+    .onGet("https://myapi-profstream.herokuapp.com/api/403281/persons")
+    .reply(200, []);
+
+    expect(shallow(<UserList />)).toMatchSnapshot();
+  });
+
+});
